d697f83e7291dfe744d5ba368f0a8314738d8ee7
Rojoin <nachoarrastua@gmail.com>
2023-11-04 18:10:17 -0300

    Add new InputActions

---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private Vector2ChannelSO inputChannel;
+        private PlayerInput _playerInput;
+
+        private void OnEnable()
+        {
+            inputChannel.Subscribe(_playerInput.actions.);
+        }
+
+        private void OnDestroy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
