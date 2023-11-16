10bc244a2546301f1a6d4a067e10241a5a30d9d8
Rojoin <nachoarrastua@gmail.com>
2023-11-16 14:23:19 -0300

    Add CuttableSO

---
+++ Assets/ScriptableObjects/CuttableSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Create Gem", menuName = "Gem", order = 0)]
+    public class CuttableSO : ScriptableObject
+    {
+        public string name;
+        public GameObject asset;
+    }
+
+    
+}
