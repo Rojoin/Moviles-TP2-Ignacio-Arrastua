3aca86d4429563a71b270d08624377698487e440
Rojoin <nachoarrastua@gmail.com>
2023-11-18 14:01:07 -0300

    Add Cutable Factory

---
+++ Assets/Scripts/Cuttables/CuttableFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace Cuttables
+{
+    public abstract class CuttableFactory
+    {
+        protected List<CuttableSO> _cuttableSos;
+
+        public abstract CuttableSO GetItem();
+        
+        
+    }
+    public class RandomCuttableFactory : CuttableFactory
+    {
+        public override CuttableSO GetItem()
+        {
+            return _cuttableSos[Random.Range(0, _cuttableSos.Count)];
+        }
+    }
+}
