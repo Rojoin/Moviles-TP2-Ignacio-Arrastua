e7517da2629ae7f03e102b45f7b26a1122160029
Rojoin <nachoarrastua@gmail.com>
2023-11-18 14:00:47 -0300

    Add Cuttable Builder

---
+++ Assets/Scripts/Cuttables/CuttableBuilder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Cuttables
+{
+    public class CuttableBuilder
+    {
+        public void ItemConfigure(GameObject CuttableItem, Vector3 position, Quaternion rotation,
+            Transform parent)
+        {
+            CuttableItem.transform.SetParent(parent);
+            CuttableItem.transform.position = position;
+            CuttableItem.transform.rotation = rotation;
+            CuttableItem.transform.localScale = Vector3.one;
+        }
+    }
+}
