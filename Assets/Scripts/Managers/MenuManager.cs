09660c3e038041917c83eb955c41cf7f9ae420a0
Rojoin <nachoarrastua@gmail.com>
2023-11-18 14:02:20 -0300

    Add Managers

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CustomSceneSwitcher.Switcher;
+using CustomSceneSwitcher.Switcher.Data;
+using Cuttables;
+using UnityEngine;
+
+namespace Managers
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField] private SceneChangeData gameScene;
+        [SerializeField]  private CuttableItem _playCuttableItem;
+
+        private void Awake()
+        {
+                _playCuttableItem.OnCut.AddListener(ChangeToPlayScene);
+        }
+
+        private void OnDestroy()
+        {
+            _playCuttableItem.OnCut.RemoveAllListeners();
+        }
+
+        private void ChangeToPlayScene(GameObject arg0)
+        {
+            Invoke(nameof(ChangeGameplay),2.0f);
+        }
+
+        private void ChangeGameplay()
+        {
+            SceneSwitcher.ChangeScene(gameScene);
+        }
+    }
+}
