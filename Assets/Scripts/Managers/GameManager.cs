98c3b05ee7d00a9ce7f53655ef47f392a02bd1a8
Rojoin <nachoarrastua@gmail.com>
2023-11-24 18:51:41 -0300

    Add GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
-        
+        private float currentTimer;
+        private int currentLives;
+        [SerializeField] private int maxLives = 3;
+        [SerializeField] private float maxTimer = 1.0f;
+        private bool isPlaying;
+        private UnityEvent OnGameFinished;
+
+
+        private void Awake()
+        {
+            currentTimer = 0;
+            currentLives = maxLives;
+        }
+
+        private void Update()
+        {
+            if (isPlaying)
+            {
+                currentTimer += Time.deltaTime;
+                if (currentTimer > maxTimer)
+                {
+                    OnGameFinished.Invoke();
+                    isPlaying = false;
+                }
+            }
+        }
     }
 }
