0fdc696308b53045bc6b735b6ca2f63d3fef64a8
Rojoin <nachoarrastua@gmail.com>
2023-11-25 22:49:13 -0300

    Add Store Manager

---
+++ Assets/Scripts/Shop/StoreManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shop
+{
+    public class StoreManager : MonoBehaviour
+    {
+        [SerializeField] private ShopItem[] _shopItems;
+        [SerializeField] private BladeSFX[] _bladeSO;
+        [SerializeField] private PlayerConfig _playerConfig;
+        [SerializeField] private Sprite blockStore;
+        [SerializeField] private Sprite availableStore;
+        [SerializeField] private Sprite inUseStore;
+
+        private void Awake()
+        {
+            _playerConfig.Init();
+            LoadShopItem();
+            var maxBlades = _bladeSO.Length;
+            for (int i = 0; i < maxBlades; i++)
+            {
+                ShopItem currentShopItem = _shopItems[i];
+                currentShopItem.Init(_bladeSO[i]);
+                if (_bladeSO[i].isAvalaible)
+                {
+                    currentShopItem.SetEquipable(availableStore);
+                    currentShopItem.onClick.AddListener(SelectItem);
+                    if (_playerConfig.CurrentBlade == currentShopItem.bladeSO.id)
+                    {
+                        currentShopItem.SetItemUsed();
+                    }
+                }
+                else
+                {
+                    currentShopItem.SetBuyableItem(blockStore);
+                    currentShopItem.onClick.AddListener(BuyItem);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (ShopItem shopItem in _shopItems)
+            {
+                shopItem.onClick.RemoveAllListeners();
+            }
+        }
+
+        private void LoadShopItem()
+        {
+            foreach (var blade in _bladeSO)
+            {
+                if (PlayerPrefs.HasKey(blade.name))
+                {
+                    blade.isAvalaible = PlayerPrefs.GetInt(blade.name) == 1;
+                }
+                else
+                {
+                    PlayerPrefs.SetInt(blade.name, blade.isAvalaible ? 1 : 0);
+                }
+            }
+        }
+
+        private void BuyItem(ShopItem shopItem)
+        {
+            if (_playerConfig.Money >= shopItem.bladeSO.price)
+            {
+                _playerConfig.Money -= shopItem.bladeSO.price;
+                shopItem.bladeSO.isAvalaible = true;
+                PlayerPrefs.SetInt(shopItem.bladeSO.name, 1);
+                shopItem.onClick.RemoveListener(BuyItem);
+                shopItem.onClick.AddListener(SelectItem);
+                shopItem.SetEquipable();
+            }
+        }
+
+        private void SelectItem(ShopItem shopItem)
+        {
+            foreach (ShopItem item in _shopItems)
+            {
+                if (item.bladeSO.id == _playerConfig.CurrentBlade)
+                {
+                    item.onClick.AddListener(SelectItem);
+                    item.SetEquipable();
+                }
+            }
+            _playerConfig.CurrentBlade = shopItem.bladeSO.id;
+            shopItem.onClick.RemoveListener(SelectItem);
+            shopItem.SetItemUsed();
+        }
+    }
+}
