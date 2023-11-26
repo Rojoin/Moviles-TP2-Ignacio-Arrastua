35bd4ad7fd8a0a34e5fe7dfafe3e905dd0ef271a
Rojoin <nachoarrastua@gmail.com>
2023-11-25 22:48:58 -0300

    Add Player config

---
+++ Assets/Scripts/Shop/PlayerConfig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shop
+{
+    [CreateAssetMenu(menuName = "Create PlayerConfig", fileName = "PlayerConfig", order = 0)]
+    public class PlayerConfig : ScriptableObject
+    {
+        public int Money
+        {
+            get => _money;
+            set
+            {
+                _money = value;
+                SaveCurrentMoney();
+            }
+        }
+        [SerializeField] private int currentBlade = 0;
+        [SerializeField] private int _money = 200;
+
+        public int CurrentBlade
+        {
+            get => currentBlade;
+            set
+            {
+                currentBlade = value;
+                SaveCurrentBlade();
+            }
+        }
+
+        public void Init()
+        {
+            Money = PlayerPrefs.GetInt("Money", Money);
+            CurrentBlade = PlayerPrefs.GetInt("CurrentBlade", CurrentBlade);
+        }
+
+        private void SaveCurrentMoney()
+        {
+            if (PlayerPrefs.HasKey("Money"))
+            {
+                PlayerPrefs.SetInt("Money", Money);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Money", Money);
+            }
+        }
+
+        private void SaveCurrentBlade()
+        {
+            PlayerPrefs.SetInt("CurrentBlade", CurrentBlade);
+        }
+    }
+}
